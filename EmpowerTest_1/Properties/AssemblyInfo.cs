0ef4880499c7c56234f8313d66ebaef02788b850
Pedro Debevere <pedro.debevere@skyline.be>
2023-03-24 11:59:12 +0100

    Add project files.

---
+++ EmpowerTest_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EmpowerTest_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("EmpowerTest_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("FAECA79F-3C63-48E0-8410-F8974611EA96")]
+[assembly: AssemblyVersion("1.0.0.0")]
